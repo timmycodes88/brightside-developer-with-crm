b470341ca673ba060b7e0cd24759a642b90dfc0a
Tim <timvan0118@gmail.com>
2023-11-13 11:16:33 +0400

    Update ladning page

--- app/(app)/crm/contacts/page.tsx
+++ app/(app)/crm/contacts/page.tsx
@@ -1,7 +1,8 @@
 "use client"
 
+import { Input } from "@/components/ui/input"
 import supabase from "@/lib/supabase"
-import { useEffect, useState } from "react"
+import { Fragment, useEffect, useState } from "react"
 import toast from "react-hot-toast"
 
 export default function Contacts() {
@@ -17,16 +18,39 @@ export default function Contacts() {
     }
     getUsers()
   }, [])
+  const [search, setSearch] = useState("")
+  const filteredUsers = users?.filter((user: any) =>
+    user.email.toLowerCase().includes(search.toLowerCase())
+  )
 
   return (
     <div>
       {loading && <div>Loading...</div>}
-      {users &&
-        users.map((user: any) => (
-          <div key={user.email} className="mb-10">
-            {JSON.stringify(user)}
-          </div>
-        ))}
+      <Input
+        value={search}
+        onChange={e => setSearch(e.target.value)}
+        placeholder="Search..."
+        className="mb-10"
+      />
+      <div className="grid grid-cols-6 gap-4">
+        <div className="font-bold">#</div>
+        <div className="font-bold">Name</div>
+        <div className="font-bold">Email</div>
+        <div className="font-bold">VIP?</div>
+        <div className="font-bold">Tags</div>
+        <div className="font-bold">Page IDs</div>
+        {users &&
+          filteredUsers.map((user: any, index: number) => (
+            <Fragment key={user.email}>
+              <div>{index + 1}</div>
+              <div>{user.first_name + " " + user.last_name}</div>
+              <div>{user.email}</div>
+              <div>{user.is_vip ? "Yes" : "No"}</div>
+              <div>View Tags</div>
+              <div>View Pages</div>
+            </Fragment>
+          ))}
+      </div>
     </div>
   )
 }
